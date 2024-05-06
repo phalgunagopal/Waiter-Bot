74ceb1b63d8895be808b2dcdca40b4fc560eb0f5
phalgunagopal <59923496+phalgunagopal@users.noreply.github.com>
2024-05-06 10:06:42 +0530

    Update table_reader.py

--- table_reader.py
+++ table_reader.py
@@ -20,7 +20,7 @@ from llama_index.core.question_gen.prompts import (
 from llama_index.core import VectorStoreIndex
 from llama_index.core.query_engine import SubQuestionQueryEngine
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
-# import streamlit as st
+import streamlit as st
 import pandas as pd
 from llama_index.core import VectorStoreIndex
 import nest_asyncio
